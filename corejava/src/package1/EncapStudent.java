047ff09fdb1aafe71eec87f51eb2c0edf963eb85
eswarsivasubramanian <eswarsivasubramanian@users.noreply.github.com>
2019-11-20 13:02:26 +0530

    20/11/2019

--- corejava/src/package1/EncapStudent.java
+++ corejava/src/package1/EncapStudent.java
@@ -1,54 +1,44 @@
 package package1;
-
 public class EncapStudent {
 	private int rollno;
 	private String name;
 	private String city;
-	
 	public String getCity() {
 		return city;
 	}
-
 	public void setCity(String city) {
 		this.city = city;
 	}
-
-	/*EncapStudent()
+	EncapStudent(String n,int rollno,String city)
 	{
-		System.out.println("constructor for student");
-	}*/
-
+		name=n;
+		this.rollno=rollno;
+		this.city=city;
+	}
 	public int getRollno() {
 		return rollno;
 	}
-
-
 	public void setRollno(int rollno) {
 		this.rollno = rollno;
 	}
-
-
 	public String getName() {
 		return name;
 	}
-
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		EncapStudent es=new EncapStudent();
-		es.setName("Eswar");
+		EncapStudent es=new EncapStudent("Eswar",1,"Chennai");
+		EncapStudent es1=new EncapStudent("Siva",2,"Chennai");
+		/*es.setName("Eswar");
 		es.setRollno(1);
-		es.setCity("Chennai");
+		es.setCity("Chennai");*/
 		System.out.println("Name: "+es.getName());
 		System.out.println("Roll No: "+es.getRollno());
 		System.out.println("City: "+es.getCity());
-
+		System.out.println("Name: "+es1.getName());
+		System.out.println("Roll No: "+es1.getRollno());
+		System.out.println("City: "+es1.getCity());
 	}
-
-	
 }
---
+++ corejava/src/package1/Test.java
@@ -0,0 +1,13 @@
+package package1;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		EncapStudent e1=new EncapStudent("Anas", 3, "Delhi");//goes for EncapStudent class
+		System.out.println("Name: "+e1.getName());
+		System.out.println("Roll No: "+e1.getRollno());
+		System.out.println("City: "+e1.getCity());
+	}
+
+}
